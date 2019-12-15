461992c6ec64461ca4801d441e37073c37bea380
Asurx <973562770@qq.com>
2019-12-15 18:59:14 +0800

    init project

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%&i=s-1ird!y_lu1&0-aei0^o5==71&t4bd_y%q(h+4^eqv&@a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.31.216','47.95.201.6',]
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.31.216', '47.95.201.6', ]
 
 
 # Application definition
