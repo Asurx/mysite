00616c01b42f15cf56fc821fa387ea195e5fdf00
Asurx <973562770@qq.com>
2019-12-16 22:30:59 +0800

    mysql

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -15,6 +15,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '%&i=s-1ird!y_lu1&0-aei0^o5==71&t4bd_y%q(h+4^eqv&@a'
+
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
