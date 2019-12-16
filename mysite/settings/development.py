0650630c12d9e612b983126146d129d9d82220b7
Asurx <973562770@qq.com>
2019-12-16 21:28:34 +0800

    mysql

--- mysite/settings/development.py
+++ mysite/settings/development.py
@@ -27,9 +27,20 @@ ALLOWED_HOSTS = ['127.0.0.1', '192.168.31.216', '47.95.201.6', ]
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysite_db',
+        'USER': 'asurx',
+        'PASSWORD': 'xb0210092piano',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
