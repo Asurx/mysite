0162f92dc36af659900bf89e74d5fe8160b62dca
Asurx <973562770@qq.com>
2019-12-20 21:59:40 +0800

    mysql

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 class BlogType(models.Model):
@@ -13,7 +13,8 @@ class BlogType(models.Model):
 class Blog(models.Model):
     title = models.CharField(max_length=50)
     blog_type = models.ForeignKey(BlogType, on_delete=models.DO_NOTHING)
-    content = models.TextField()
+    # content = models.TextField()
+    content = RichTextField()
 
     author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
 
